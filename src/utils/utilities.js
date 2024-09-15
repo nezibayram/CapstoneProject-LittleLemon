53087f46d5b7a54fe90facbcd1ccb79b6a78b0f2
Nezihe Bayram <nezihe.bayram22@gmail.com>
2024-09-15 20:54:09 +0200

    Create utilities.js

---
+++ src/utils/utilities.js
@@ -0,0 +1,20 @@
+export const occasions = [
+    '',
+    'Birthday',
+    'Anniversary',
+    'Family Gathering',
+    'Business Meeting',
+    'Graduation',
+    "Friends'Reunion",
+    'Engagement',
+  ];
+  
+  export const initialFormState = {
+    name: '',
+    email: '',
+    time: '',
+    date: '',
+    bookingOccasion: '',
+    numberOfGuests: '1',
+  };
+  
